63d21172896ea9bf1a1f012a918da97fc9ccbb35
Rainer Borene <rainerborene@gmail.com>
2015-11-05 08:50:13 -0200

    Clean up settings controller spec.

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -2,24 +2,20 @@ require 'rails_helper'
 
 describe SettingsController, type: :controller do
   context 'when logged in', vcr: { cassette_name: :valid_credentials } do
-    let(:jane_doe) { Fabricate.attributes_for(:jane_doe) }
-    let(:current_user) { controller.current_user }
     let(:aws_params) { %i(aws_access_key_id aws_secret_access_key aws_region) }
     let(:profile_params) { %i(name email password language time_zone) }
+    let(:account_params) do
+      { account: Fabricate.attributes_for(:jane_doe) }
+    end
 
     before do
       account = Fabricate.build(:account)
-
-      allow_any_instance_of(Account).to receive(:update) do |record, params|
-        record.assign_attributes(params)
-        true
-      end
-
+      expect(account).to receive(:update).at_most(:twice)
       sign_in_as account
     end
 
     describe 'PUT /profile' do
-      before { put :profile, account: jane_doe }
+      before { put :profile, account_params }
 
       it { is_expected.to use_before_action :require_login }
       it { is_expected.to respond_with :success }
@@ -30,7 +26,7 @@ describe SettingsController, type: :controller do
     end
 
     describe 'PUT /aws' do
-      before { put :aws, account: jane_doe }
+      before { put :aws, account_params }
 
       it { is_expected.to use_before_action :require_login }
       it { is_expected.to respond_with :success }
@@ -41,8 +37,8 @@ describe SettingsController, type: :controller do
     end
 
     def permit_params(params)
-      options = { verb: :put, params: { account: jane_doe } }
-      permit(*params).for(controller.action_name, options).on(:account)
+      permit(*params).on(:account)
+        .for(controller.action_name, verb: :put, params: account_params)
     end
   end
 end

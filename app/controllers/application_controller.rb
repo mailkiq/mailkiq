166bac4eb02d54120d29a69a8d76e49e46bfdaf6
Rainer Borene <rainerborene@gmail.com>
2016-02-01 09:55:14 -0200

    Send current user ID to Sentry.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   responders :flash
   respond_to :html
+  prepend_before_action :set_raven_context
+
+  def set_raven_context
+    Raven.user_context user_id: current_user.id if signed_in?
+  end
 end

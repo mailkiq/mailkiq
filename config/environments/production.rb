bc6439342ae6084ad972caa61618881bb2cd74b6
Rainer Borene <rainerborene@gmail.com>
2016-02-03 23:31:44 -0200

    Add default_url_options option for ActionMailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,4 +14,5 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'mailkiq.com' }
 end

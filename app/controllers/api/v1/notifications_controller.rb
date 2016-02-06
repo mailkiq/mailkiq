0e33c8b65941cb8dc2199a25a77a6d6854ae498c
Rainer Borene <rainerborene@gmail.com>
2016-02-06 11:23:19 -0200

    Remove comments.

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -3,24 +3,16 @@ require_dependency 'sns'
 
 module API::V1
   class NotificationsController < BaseController
-    # before_action :require_amazon_headers
     before_action :require_token
 
     def create
-      # TODO: can't lose any notification!!!
-      # TODO: move to a background process
-      # TODO: measure all the things!
-
-      # Verify the authenticity of messages.
       @message_body = request.body.read
       @verifier = SNS::MessageVerifier.new
       @verifier.authenticate! @message_body
 
-      # Parse message
       @json = JSON.parse(@message_body)
       @notification = SNS::Notification.new @json
 
-      # Automatic subscription confirmation.
       if @notification.subscription_confirmation?
         sns = Fog::AWS::SNS.new(@account.credentials)
         sns.confirm_subscription @notification.topic_arn, @notification.token
@@ -32,11 +24,6 @@ module API::V1
     end
 
     private
-
-    # def require_amazon_headers
-    #   TODO: Validate X-Amz-Sns-Message-Type header.
-    #   TODO: Validate X-Amz-Sns-Topic-Arn header.
-    # end
 
     def require_token
       @decoded_token = Token.decode params.require(:token)

99df9d6212cd2a2ffdda3fbf272ef38cec2ad0cb
Rainer Borene <rainerborene@gmail.com>
2016-02-17 23:52:06 -0200

    Add percentage badge tags.

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -1,4 +1,9 @@
 module BootstrapHelper
+  def page_title
+    naming = PageMeta::Naming.new(controller)
+    t("page_meta.titles.#{naming.controller}.#{naming.action}")
+  end
+
   def nav_link_to(name, path)
     css_class = 'active' if request.path == path
     content_tag :li, link_to(name, path), role: 'presentation', class: css_class
@@ -10,8 +15,14 @@ module BootstrapHelper
     link_to tag(:span, class: "glyphicon glyphicon-#{icon}"), path, options
   end
 
-  def page_title
-    naming = PageMeta::Naming.new(controller)
-    t("page_meta.titles.#{naming.controller}.#{naming.action}")
+  def trash_link_to(path)
+    icon_link_to path, icon: :trash, method: :delete,
+                       data: { confirm: t('actions.confirm') }
+  end
+
+  def percentage_badge_tag(number, total)
+    value = number.to_f * 100 / (total.zero? ? 1 : total)
+    percentage = number_to_percentage(value, precision: 1)
+    content_tag :span, percentage, class: 'label label-default'
   end
 end

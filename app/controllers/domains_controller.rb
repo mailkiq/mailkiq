b9c6173465fe75df7b195023ef5e610bc498df2e
Rainer Borene <rainerborene@gmail.com>
2016-03-09 12:15:55 -0300

    Handle domain validation error message.

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -4,13 +4,15 @@ class DomainsController < ApplicationController
   def create
     @domain = current_user.domains.new domain_params
     @domain.save
-    respond_with @domain, location: amazon_settings_path
+    respond_with @domain, flash_now: false do |format|
+      format.html { redirect_to amazon_settings_path }
+    end
   end
 
   def destroy
     @domain = current_user.domains.find params[:id]
     @domain.destroy
-    respond_with @domain, location: amazon_settings_path
+    respond_with @domain, flash_now: false, location: amazon_settings_path
   end
 
   private

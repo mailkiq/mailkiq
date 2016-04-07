471f4f972d13ca42413d36b1921bb399417cc7e9
Rainer Borene <rainerborene@gmail.com>
2016-04-07 20:50:04 -0300

    Enable DKIM after domain is verified.

--- app/workers/domain_worker.rb
+++ app/workers/domain_worker.rb
@@ -5,13 +5,20 @@ class DomainWorker
 
   def perform(account_id)
     @account = Account.find account_id
-    @account.domains.each { |domain| update_verification_attributes(domain) }
+    @account.domains.each do |domain|
+      update_verification_attributes(domain)
+      enable_identity_dkim(domain) if domain.success?
+    end
   end
 
   private
 
   def ses
     @ses ||= Aws::SES::Client.new(@account.credentials)
+  end
+
+  def enable_identity_dkim(domain)
+    ses.set_identity_dkim_enabled identity: domain.name, dkim_enabled: true
   end
 
   def update_verification_attributes(domain)
--- spec/workers/domain_worker_spec.rb
+++ spec/workers/domain_worker_spec.rb
@@ -4,16 +4,20 @@ describe DomainWorker, type: :worker do
   it { is_expected.to save_backtrace }
   it { is_expected.to be_processed_in :platform }
 
-  it 'update verficaition attributes', vcr: { cassette_name: :get_identity_verification_attributes } do
+  it 'update verification attributes', vcr: { cassette_name: :get_identity_verification_attributes } do
     account = Fabricate.build :valid_account
     domain = Fabricate.build :domain
 
     expect(account).to receive(:domains).and_return([domain])
     expect(Account).to receive(:find).with(1).and_return(account)
     expect(domain).to receive(:update_columns).with(
-      status: Domain.statuses[:pending],
+      status: Domain.statuses[:success],
       verification_token: 'G8qYlxUb5fkAus3eY/tp83XPKI0RvChrEfjYl4aEn7s='
     )
+
+    expect_any_instance_of(Aws::SES::Client)
+      .to receive(:set_identity_dkim_enabled)
+      .with(identity: domain.name, dkim_enabled: true)
 
     subject.perform(1)
   end

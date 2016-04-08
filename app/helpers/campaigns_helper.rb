655b03632c15c3069d0c5f6855255dd4ef538bd7
Rainer Borene <rainerborene@gmail.com>
2016-04-08 14:47:08 -0300

    Minor improvements.

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -3,8 +3,7 @@ module CampaignsHelper
     count = campaign.send(counter_name).to_f
     total = (campaign.recipients_count.zero? ? 1 : campaign.recipients_count)
     value = count * 100 / total
-    percentage = number_to_percentage(value, precision: value.zero? ? 0 : 1)
-
+    percentage = number_to_percentage(value, precision: value.zero? || value >= 10 ? 0 : 1)
     content_tag :span, percentage, class: 'campaign-percentage'
   end
 

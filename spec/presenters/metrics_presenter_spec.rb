b7b58085b0753f6420013060e2763c1e8bea7a97
Rainer Borene <rainerborene@gmail.com>
2016-04-14 22:12:48 -0300

    Add specs for MetricsPresenter class.

--- spec/presenters/metrics_presenter_spec.rb
+++ spec/presenters/metrics_presenter_spec.rb
@@ -10,10 +10,41 @@ describe MetricsPresenter do
 
   describe '#delivered' do
     it 'calculates percentage of emails delivered until now' do
+      is_expected.to receive(:percentage_for).with(:messages_count)
+        .and_call_original
+
       expect(campaign).to receive(:recipients_count).and_return(646_100)
       expect(campaign).to receive_message_chain(:messages_count, :value)
         .and_return(27_137)
       expect(subject.delivered).to eq('4%')
+    end
+  end
+
+  describe '#bounces' do
+    it 'calculates percentage of bounced emails' do
+      is_expected.to receive(:percentage_for).with(:bounces_count)
+      subject.bounces
+    end
+  end
+
+  describe '#complaints' do
+    it 'calculates percentage of complained emails' do
+      is_expected.to receive(:percentage_for).with(:complaints_count)
+      subject.complaints
+    end
+  end
+
+  describe '#rejects' do
+    it 'calculates percentage of rejected emails' do
+      is_expected.to receive(:percentage_for).with(:rejects_count)
+      subject.rejects
+    end
+  end
+
+  describe '#unsent' do
+    it 'calculates percentage of unsent emails' do
+      is_expected.to receive(:percentage_for).with(:unsent_count)
+      subject.unsent
     end
   end
 end

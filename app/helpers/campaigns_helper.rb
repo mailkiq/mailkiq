94d6d47d3777b59633ee5e4b01ec9a4e7f5680ed
Rainer Borene <rainerborene@gmail.com>
2016-04-14 22:49:18 -0300

    Fix precision on float divisions.

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,15 +1,15 @@
 module CampaignsHelper
   def campaign_percentage_tag(campaign, counter_name, css_class: nil)
-    count = campaign.send(counter_name).to_f
     total = (campaign.recipients_count.zero? ? 1 : campaign.recipients_count)
-    value = count * 100 / total
+    value = campaign.send(counter_name).to_f * 100 / total
     precision = value.zero? || value >= 10 ? 0 : 1
     content_tag :span, number_to_percentage(value, precision: precision),
                 class: css_class || 'campaign-percentage'
   end
 
   def campaign_meter_tag(campaign, counter_name)
-    width = campaign.send(counter_name).value / campaign.recipients_count * 100
+    width = campaign.send(counter_name).value / campaign.recipients_count.to_f * 100
+    width = width.round(1)
     content_tag :div, class: :meter do
       content_tag :span, nil, style: "width: #{width}%"
     end

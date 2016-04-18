583001e8d0f029d901410a0c13707a266f127302
Rainer Borene <rainerborene@gmail.com>
2016-04-18 17:38:26 -0300

    Update Devise to version 4

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,4 +12,5 @@ Devise.setup do |config|
   config.password_length = 8..72
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
+  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
 end

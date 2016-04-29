d0a0fd19f3fcaf061ed06a97aa8818b6e683d8f4
Rainer Borene <rainerborene@gmail.com>
2016-04-29 16:40:18 -0300

    Remove empty queues periodically.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,10 @@
 require 'dynamic_fetch'
+require 'queue_monitor'
 
 Sidekiq.options[:fetch] = DynamicFetch
+
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    QueueMonitor.start
+  end
+end

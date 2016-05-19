7c1acbae7a99adf3f2cb2948f899181b685bbfbd
Rainer Borene <rainerborene@gmail.com>
2016-05-19 13:17:57 -0300

    Improve ContactsController tests.

--- spec/controllers/api/v1/contacts_controller_spec.rb
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -7,19 +7,21 @@ describe API::V1::ContactsController, type: :controller do
       let(:params) do
         data = json :clickfunnels
         data[:api_key] = account.api_key
+        data[:tag] = 'blog'
         data
       end
 
       let(:digest) do
         url = api_v1_contacts_url(params)
-        Digest::MD5.hexdigest("#{url}#{params.except(:api_key).to_query}")
+        Digest::MD5.hexdigest("#{url}#{params.except(:api_key, :tag).to_query}")
       end
 
       before do
+        relation = double('relation')
+
         request.headers['X-Clickfunnels-Webhook-Delivery-Id'] = digest
 
         allow(ActiveRecord::Base).to receive(:transaction).and_yield
-        expect_sign_in_as account
         expect_any_instance_of(Subscriber).to receive(:valid?).and_return(true)
         expect_any_instance_of(Subscriber).to receive(:save) do |model|
           model.run_callbacks :create
@@ -27,6 +29,12 @@ describe API::V1::ContactsController, type: :controller do
 
         expect_any_instance_of(API::V1::SubscriberResource)
           .to receive(:redefine_model)
+
+        expect(relation).to receive(:where).with(slug: 'blog')
+          .and_return(relation)
+        expect(relation).to receive(:pluck).with(:id).and_return([])
+        expect(account).to receive(:tags).and_return(relation)
+        expect_sign_in_as account
 
         post :create, params
       end
